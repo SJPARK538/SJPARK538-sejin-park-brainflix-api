8b01a53fbc159d32f13a11f9ab72bca738ddc9d7
sjpark538 <phi5388@gmail.com>
2022-06-23 19:59:52 -0400

    selected video server updated

--- routes/videos.js
+++ routes/videos.js
@@ -1,22 +1,47 @@
 const express = require("express");
-const videosFile = __dirname + "/../../data/videos.json";
-const videos = require(videosFiles);
+const videosFile = __dirname + "/../data/videos.json";
+const videos = require(videosFile);
 const router = express.Router();
+
+
+// router.get("/:id", (req, res) => {
+
+//     console.log('req', req.params)
+//     const item = videos.find(video => video.id === req.params.id);
+//     console.log('item', item);
+//     res.json(item)
+//     }
+// )
 
 router.get ("/", (req, res)=>{
     const videoLists = videos.map(video => {
         return {
             id: video.id,
-            title: video.title,
+            title: video.title, 
             channel: video.channel,
             image: video.image,
         }
     })
+    console.log('videoLists', videoLists);
     res.json(videoLists);
 });
 
-router.get("/:id", (req, res) => {
-    const find = videos.filter
-})
+router.get ("/:id", (req, res) => {
+    console.log('req', req.params.id)
+
+    const item = videos.find(video => video.id === req.params.id);
+    console.log('item', item);
+    // res.send(item)
+    if(item){
+        res.status(200)
+            .json(item)
+    }else {
+        res.status(400)
+        .json({errorMessage: `Video ID ${req.params.id} not found`})
+
+    }
+    }
+)
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || process.argv[2] || 8080;
-const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 
+const videos = require('./data/videos.json');
+
 app.use(cors());
+
 app.use(express.json());
+
+const videoRoutes = require("./routes/videos")
+app.use("/api/videos", videoRoutes);
+
+
+// app.get('/:id', (req, res) => {
+
+//     console.log('req', req.params)
+//     const item = videos.find(video => video.id === req.params.id);
+//     console.log('item', item);
+//     res.send(item)
+//     }
+// )
 
 
 
 console.log("hi");
+
+
+
+
+app.listen(port, () => console.log(`Listening on ${port}`));
 
